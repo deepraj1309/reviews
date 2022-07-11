8219057b392fdeaaf31b861fcca30824c992a522
Deeprajsinh Gohil <deeprajsinhgohil@Deeprajsinhs-MacBook-Air.local>
2022-07-11 14:32:45 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
-import requests
+import requests 
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
